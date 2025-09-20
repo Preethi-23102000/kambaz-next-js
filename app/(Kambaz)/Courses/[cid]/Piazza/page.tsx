a07cc1b464437078230a005cbd1e772a8b5b966c
Preethi-23102000 <preethi.yennemadi@gmail.com>
2025-09-19 23:52:32 -0400

    Assignment A1 - clean up - adding placeholders for zoom piazza and quizes

---
+++ app/(Kambaz)/Courses/[cid]/Piazza/page.tsx
@@ -0,0 +1,7 @@
+export default function Piazza() {
+  return (
+    <div id="wd-piazza">
+      <h1 id="wd-piazza-title">Piazza Placeholder</h1>
+    </div>
+  );
+}
---
+++ app/(Kambaz)/Courses/[cid]/Quizzes/page.tsx
@@ -0,0 +1,7 @@
+export default function Quizzes() {
+  return (
+    <div id="wd-quizzes">
+      <h1 id="wd-quizzes-title">Quizzes Placeholder</h1>
+    </div>
+  );
+}
