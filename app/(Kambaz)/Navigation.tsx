dbe6f9e8c31b3fc03ebdc69a5c6f1aeb20a2aa10
Preethi-23102000 <preethi.yennemadi@gmail.com>
2025-10-05 20:36:43 -0400

    Assignment 2 - testing deployment

--- app/(Kambaz)/Navigation.tsx
+++ app/(Kambaz)/Navigation.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
-import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { LiaBookSolid } from "react-icons/lia";
 import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { ListGroup, ListGroupItem } from "react-bootstrap";
 import { GoBeaker } from "react-icons/go";
